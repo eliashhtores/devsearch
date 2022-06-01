96ac62cec8cdd4bb07f0f8b7a7d720a6b96185ca
eliashhtorres <eliashhtorres@gmail.com>
2022-06-01 07:33:32 -0500

    Added S3 bucket

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -152,3 +152,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if os.getcwd() == '/app':
+    DEFAULT_FILE_STORAGE = env('DEFAULT_FILE_STORAGE')
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
